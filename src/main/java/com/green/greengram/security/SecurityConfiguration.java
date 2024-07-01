54401648efd73d82d12919e9736468829b45432e
Administrator <wltngus200@gmail.com>
2024-07-01 20:19:59 +0900

    0627-28 깃허브 내용 카피

--- src/main/java/com/green/greengram/security/SecurityConfiguration.java
+++ src/main/java/com/green/greengram/security/SecurityConfiguration.java
@@ -16,7 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration //항상 bean이 붙은 메소드가 존재 함
 @RequiredArgsConstructor
 public class SecurityConfiguration {
-    private final JwtAuthenticationFilter jwtAuthenticationFilter; //DI(@Configuration으로 빈등록 되어있음)
+    private final JwtAuthenticationFilter jwtAuthenticationFilter; //DI(@Component로 빈등록 되어있음)
 
 
     /*Security와 관련된 빈등록을 여러개 -> 메소드 레벨 빈등록=나중에 취합하기 좋음
@@ -29,7 +29,7 @@ public class SecurityConfiguration {
 
         //파라미터 없이 내가 직접 new 객체화 해서 리턴으로 빈등록 가능
 
-        //람다식~
+
 
        /* 이거 뭐지..?
             return httpSecurity.sessionManagement(new Customizer<SessionManagementConfigurer<HttpSecurity>>()){
@@ -40,15 +40,16 @@ public class SecurityConfiguration {
         }*/
 
 
-        return httpSecurity.sessionManagement(session ->
+        return httpSecurity.sessionManagement(session -> /*람다식*/
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS/*상태 없음*/)) //jwt를 할 것이기 때문에 필요 X
                                                     //세팅: 시큐리티에서 세션 사용하지 않겠다(람다식)
                 .httpBasic(http->http.disable())
+                //SSR 하지 않는다, html 화면을 백엔드가 만들지 않는다
                 //백엔드에서 만들지 않더라도 위 세팅을 끄지 않아도 괜찮다. 사용하지 않는 걸 끔으로써 리소스 확보 위함
                 //스프링에서 제공하는 로그인 화면을 사용하지 않겠다. 왜? 안 예뻐서 ㅋㅋㅋㅋㅋㅋㅋㅋ
                 .formLogin(form->form.disable()) //form 로그인 방식 사용 X SSL X html 화면을 백엔드가 만들지 않아 필요 X
                 //시큐리티에서 폼 로그인 할 때 보안문제로 HTML 폼테그 작성시 뭔가 더 적음
-                //폼 로그인할 때 내가 보내준 문서(화면)에서 했는지..
+                //form 로그인할 때 내가 보내준 문서(화면)에서 했는지..
                 .csrf(csrf->csrf.disable()) //CSRF(cors와 많이 헷갈려함)  //사이트간 요청 위조  끄는 이유: 로그인 방식이 세션일때만 가능(우리는 토큰)
                 //cross site Request forgery 이걸 막겠다고 안 해놓은 사이트>> 이 전페이지가 자신의 페이지였는지 확인하는 용
                 // A->B로 이체 하는 요청 (요청은 알아내기 쉬움) A사용자에게 링크를 보내면 그 링크를 누르면 요청이 날아가게
@@ -59,16 +60,24 @@ public class SecurityConfiguration {
                 //백에서는 cors가 싫다고만 할 수 있음(브라우저에서 막지만, 브라우저 설정(옵션)을 바꾸면 가능)
                 //백엔드가 준 문서에서 요청이 들어오는 것은 안 막겠다, 다른 사이트에서 오는 요청은 싫어!
                 //내가 준 HTML문서에서 요청이 들어오면 좋고 아니면 싫음
-                .authorizeHttpRequests(auth->auth.requestMatchers("/api/user/sign-up",/*로그인 되어있지 않아도 사용 가능해야함*/ "/api/user/sign-in",
-                                "/swagger","/swagger-ui/**", "/v3/api-docs/**", //스웨거 보이게
-                                "/pic/**", //사진 WebMvcConfigurer 참고
+                .authorizeHttpRequests(auth->auth.requestMatchers(
+                                /*로그인 되어있지 않아도 사용 가능해야함*/
+                                "/api/user/sign-up","/api/user/sign-in", "/api/user/access-token",
+                                /*스웨거 보이게*/
+                                "/swagger","/swagger-ui/**", "/v3/api-docs/**",
+                                /*사진 WebMvcConfigurer 참고*/
+                                "/pic/**", "/fimg/**",
+                                /*프론트 화면 보일 수 있게*/
                                 "/", "/index.html"/*골격*/,
-                                "/css/**"/*화면 색, 위치 등을 관장*/,"/js/**"/*이러한 동작을 했을때 이렇게 하자!*/,"/static/**", //프론트 화면 보이게
-                        //자바스크립트가 없으면 두가지는 정적이 됨
-                        //프론트가 따로 서버를 돌리면 이 작업을 하지 않아도 됨
-                        "/sign-up", "sign-in", "/profile/*"/*1차까지만 허용*/, "/feed")//프론트의 라우터 주소
-                        .permitAll()//로그인 해야만 사용할 수 있다 로그인 하지 않아도 사용할 수 있다. 권한이 있는 자만 사용할 수 있다
-                        .anyRequest()/*어떤 요청이든*/.authenticated())//순서가 중요  url을 설정한 부분이 any 뒤에 있으면 적용 X
+                                "/css/**"/*화면 색, 위치 등을 관장*/,"/js/**"/*이러한 동작을 했을때 이렇게 하자!*/,"/static/**",/*프론트 화면 보이게/*
+                                //자바스크립트가 없으면 두가지는 정적이 됨
+                                //프론트가 따로 서버를 돌리면 이 작업을 하지 않아도 됨
+                                /*프론트의 라우터 주소*/
+                                "/sign-up", "sign-in", "/profile/*"/*1차까지만 허용*/, "/feed",
+                                /*actuator*/
+                                "/actuator", "/actuator/*")
+                        .permitAll()//로그인 하지 않아도 사용할 수 있다. //체이닝 기법
+                        .anyRequest()/*어떤 요청이든*/.authenticated()/*권한이 있는 자만 사용할 수 있다*/)//순서가 중요 url을 설정한 부분이 any 뒤에 있으면 적용 X
                 //스웨거가 안 뜬다는 부작용 존재 //이미지가 안 뜬다는 부작용(백에서 요청받아서 뜨는 이미지, 프론트랑 상관 X)
                 //이젠 화면이 안 뜸 localhost8080 스테틱에 있는 index.html이 실행 안 되기 때문 "/"를 추가, index.html도 추가
                 //자바스크립트 파일도 다 막힘
@@ -102,3 +111,18 @@ public class SecurityConfiguration {
     }
 
 }
+
+ /* 체이닝 기법
+
+    만약, permitAll메소드가 void였다면 아래와 같이 작성을 해야함
+    .authorizeHttpRequests(auth -> {
+
+        auth.requestMatchers("/api/user/sign-up","/api/user/sign-in").permitAll();
+        auth.requestMatchers("/api/ddd").authenticated();
+    })
+    permitAll 메소드가 자기 자신의 주소값을 리턴한다면 체이닝 기법 가능
+    .authorizeHttpRequests(auth -> {
+        auth.requestMatchers("/api/user/sign-up","/api/user/sign-in").permitAll()
+            .requestMatchers("/api/ddd").authenticated();
+    })
+                */
