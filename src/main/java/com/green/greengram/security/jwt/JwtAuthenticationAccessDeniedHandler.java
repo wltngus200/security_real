3f413fb8c188fd7ddcc77b1344103d977010bee1
Administrator <wltngus200@gmail.com>
2024-07-02 08:56:36 +0900

    0701 tmp

--- src/main/java/com/green/greengram/security/jwt/JwtAuthenticationAccessDeniedHandler.java
+++ src/main/java/com/green/greengram/security/jwt/JwtAuthenticationAccessDeniedHandler.java
@@ -11,7 +11,9 @@ import java.io.IOException;
 //권한 없는 사람이 접근시 처리
 public class JwtAuthenticationAccessDeniedHandler implements AccessDeniedHandler {
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException/*인증 거부 오류*/ accessDeniedException) throws IOException/*입출력 오류*/, ServletException {
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException/*인증 거부 오류*/ accessDeniedException)
+                        throws IOException/*입출력 오류*/, ServletException {
         response.sendError(HttpServletResponse.SC_FORBIDDEN); //403에러 리턴
     }
 }
